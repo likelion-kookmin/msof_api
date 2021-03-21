7bbfcfa45c95142732ad337d721b84837bea702a
shinkeonkim <singun11@kookmin.ac.kr>
2021-03-21 16:27:40 +0900

    feat: 0005 base_model

---
+++ msof_api/base_model.py
@@ -0,0 +1,38 @@
+import datetime
+
+from django.db import models
+
+class BaseModelManager(models.Manager):
+    # 해당 옵션은 기본 매니저로 이 매니저를 정의한 모델이 있을 때 이 모델을 가리키는 모든 관계 참조에서 모델 매니저를 사용할 수 있도록 한다.
+    use_for_related_fields = True
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+class BaseModel(models.Model):
+    objects = BaseModelManager()
+
+    created_at = models.DateTimeField(
+        verbose_name='추가된 날짜',
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        verbose_name='수정된 날짜',
+        auto_now=True
+    )
+    deleted_at = models.DateTimeField(
+        verbose_name='삭제된 날짜',
+        null=True,
+        default=None
+    )
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted_at = datetime.datetime.now()
+        self.save(update_fields=['deleted_at'])
+
+    def restore(self):
+        self.deleted_at = None
+        self.sace(update_fields=['deleted_at'])
