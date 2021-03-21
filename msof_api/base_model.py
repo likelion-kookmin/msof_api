1099dc6c505dc5011ee130ebc630218063e3d00d
shinkeonkim <singun11@kookmin.ac.kr>
2021-03-21 17:21:22 +0900

    chore: 0005 코드 리뷰 반영

--- msof_api/base_model.py
+++ msof_api/base_model.py
@@ -3,7 +3,8 @@ import datetime
 from django.db import models
 
 class BaseModelManager(models.Manager):
-    # 해당 옵션은 기본 매니저로 이 매니저를 정의한 모델이 있을 때 이 모델을 가리키는 모든 관계 참조에서 모델 매니저를 사용할 수 있도록 한다.
+    # NOTE: 해당 옵션은 기본 매니저로 이 매니저를 정의한 모델이 있을 때 이 모델을 가리키는 모든 관계 참조에서 모델 매니저를 사용할 수 있도록 한다.
+    # 참고링크: https://velog.io/@kim6515516/장고에서-모델-소프트-삭제-구현하기
     use_for_related_fields = True
 
     def get_queryset(self):
