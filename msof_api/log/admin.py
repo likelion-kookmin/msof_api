b08a2c9001dd3b4c4559b6f28d1ff733ab677235
leekijung <rlwjd4177@naver.com>
2021-03-23 13:19:50 +0900

    add: Activity Model

--- msof_api/log/admin.py
+++ msof_api/log/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Activity
-# Register your models here.
-
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
--- msof_api/log/models.py
+++ msof_api/log/models.py
@@ -2,40 +2,24 @@ from django.db import models
 # from msof_api.base_model import BaseModel
 from msof_api.users.models import User
 
-class Activity(models.Model):
-    
-    ACTION_CHOICE = (
-        ('질문 등록', 'Add_Question'),
-        ('댓글 작성', 'Add_Comment'),
-        ('댓글 채택', 'Select_Comment'),
-        ('채택받은 댓글', 'Selected_Commend'),
-        ('회원가입', 'SignUp'),
-        ('좋아요받은 질문', 'Liked_Question'),
-        ('error', 'error')
-    )
+# ACTION_CHOICE = (
+#         ('질문 등록', 'Add_Question'),
+#         ('댓글 작성', 'Add_Comment'),
+#         ('댓글 채택', 'Select_Comment'),
+#         ('채택받은 댓글', 'Selected_Commend'),
+#         ('회원가입', 'SignUp'),
+#         ('좋아요받은 질문', 'Liked_Question'),
+#         ('error', 'error')
+#     )
 
+class PointRule(models.Model):
+    point = models.IntegerField()
+    name = models.CharField()
+
+class Activity(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    point = models.IntegerField(default=0)
-    activity = models.CharField(max_length = 20, choices=ACTION_CHOICE, default='error')
+    point_rule = models.ForeignKey(PointRule, on_delete = models.CASCADE)
 
     # TODO: str에 created를 추가해줘야함. 
-    def __str__(self) :
-        return "{0}님이 {1} 행동을 통해 {2}점을 얻었습니다.".format(self.user.username, self.activity, self.point)
-
-    def save(self, *args, **kwargs):
-        if self.activity == "질문 등록" :
-            self.point = 5
-        elif self.activity == "댓글 작성" :
-            self.point = 1
-        elif self.activity == "댓글 채택" :
-            self.point = 1
-        elif self.activity == "채택받은 댓글" :
-            self.point = 5
-        elif self.activity == "회원가입" :
-            self.point = 10
-        elif self.activity == "좋아요받은 질문" :
-            self.point = 0.5
-        elif self.activity == "error" :
-            self.point = 0
-        return super().save(*args, **kwargs)
-    # 
+    def __str__(self):
+        return "{0}님이 {1} 행동을 통해 {2}점을 얻었습니다.".format(self.user.username, self.point_rule.name, self.point_rule.point)
