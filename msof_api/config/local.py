8da41479d0ae0fd0aa7a994c01ad4f3e35c18142
Aqudi <gjdigj145@gmail.com>
2021-05-02 02:49:30 +0900

    bugfix: Add django-cors-headers middleware
    - 패키지 추가
    - local 세팅에 CORS 모두 허용으로 설정
    Close #28

--- msof_api/config/local.py
+++ msof_api/config/local.py
@@ -1,3 +1,4 @@
+"""msof_api local setting 파일"""
 import os
 
 from .common import Common
@@ -6,11 +7,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Local(Common):
+    """Common 설정을 상속받은 Local 설정 클래스"""
+
     DEBUG = True
 
     # Testing
     INSTALLED_APPS = Common.INSTALLED_APPS
-    INSTALLED_APPS += ('django_nose',)
+    INSTALLED_APPS += ('django_nose', 'corsheaders',)
+    MIDDLEWARE = Common.MIDDLEWARE
+    MIDDLEWARE += ('corsheaders.middleware.CorsMiddleware',)
     TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
     NOSE_ARGS = [
         BASE_DIR,
@@ -25,3 +30,7 @@ class Local(Common):
     EMAIL_HOST = 'localhost'
     EMAIL_PORT = 1025
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    # Allow CORS
+    CORS_ORIGIN_ALLOW_ALL = True
+    CORS_ALLOW_CREDENTIALS = True
