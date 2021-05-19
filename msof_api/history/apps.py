9863a5b0d2318f52e21f53a4a8bfb1f14ed6f172
shinkeonkim <singun11@kookmin.ac.kr>
2021-05-19 18:39:54 +0900

    feat: Viewed 모델 구성 및 어드민 사이트 설정

---
+++ msof_api/history/apps.py
@@ -0,0 +1,10 @@
+""" # history/apps.py
+
+    history 앱의 기본 설정을 포함하고 있습니다.
+"""
+from django.apps import AppConfig
+
+
+class HistoryConfig(AppConfig):
+    """ # HistoryConfig """
+    name = 'history'
