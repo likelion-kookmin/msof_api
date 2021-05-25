bcb4b82331e20ec4322b8f5df27bf16963a9927f
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2021-05-25 10:28:03 +0900

    feat:Universty_model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 """account 앱, User,University model 등록"""
 from django.contrib import admin
 
-from .models import Univerisity, User
+from .models import University, User
 
 
 @admin.register(User)
@@ -11,8 +11,8 @@ class UserAdmin(admin.ModelAdmin):
     """
 
 
-@admin.register(Univerisity)
-class UniverisityAdmin(admin.ModelAdmin):
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
     """아직 어떤거 customizing 해야 할지 모르겠음
     일단 만들어 놓음
     """
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ request에 따른 response를 처리하기 위한 모듈
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 
-from .serializers import UserSerializer
+from .models import University
+from .serializers import UniversitySerializer, UserSerializer
 
 User = get_user_model()
 
@@ -17,3 +18,12 @@ class UserViewSet(viewsets.ModelViewSet):  # pylint: disable=R0901&R0903
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UniversityViewSet(viewsets.ModelViewSet):  # pylint: disable=R0901&R0903
+    """UserViewSet
+    User 모델에 대한 기본적인 GET,POST,PUT,DELETE 메소드 지원
+    ModelViewSet 자체가 많은 view 관련 class를 상속받기 때문에
+    pylint: disable=R0901를 안 걸어주면 pylint에서 many ancenstor 걸림
+    """
+    queryset = University.objects.all() # pylint: disable=E1101
+    serializer_class = UniversitySerializer
