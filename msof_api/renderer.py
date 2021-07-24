dc64d57842f0569cfc6c8d8af7d2942a38de5586
shinkeonkim <singun11@kookmin.ac.kr>
2021-07-25 01:31:31 +0900

    fix: renderer 사용을 안 하는 response를 주도록 수정

--- msof_api/renderer.py
+++ msof_api/renderer.py
@@ -1,3 +1,5 @@
+from rest_framework.compat import (INDENT_SEPARATORS, LONG_SEPARATORS,
+                                   SHORT_SEPARATORS)
 from rest_framework.renderers import JSONRenderer
 from rest_framework.utils import json
 
@@ -8,19 +10,38 @@ class JSONResponseRenderer(JSONRenderer):
     charset = 'utf-8'
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        if data is None:
+            return b''
+
+        renderer_context = renderer_context or {}
+        indent = self.get_indent(accepted_media_type, renderer_context)
+
+        if indent is None:
+            separators = SHORT_SEPARATORS if self.compact else LONG_SEPARATORS
+        else:
+            separators = INDENT_SEPARATORS
+
         status_code = renderer_context['response'].status_code
         success = str(status_code)[0:2] == '20'
 
         response_dict = {
             'success': success,
+            'next': data['next'] if 'next' in data else None,
+            'previous': data['previous'] if 'previous' in data else None,
             'status code': status_code,
             'data': data['results'] if 'results' in data else data,
-            'message': '',
+            'message': ''
         }
-        if 'next' in data:
-            response_dict['next'] = data['next']
-        if 'previous' in data:
-            response_dict['previous'] = data['previous']
 
         data = response_dict
-        return json.dumps(data)
+
+        ret = json.dumps(
+            data,
+            cls=self.encoder_class,
+            indent=indent,
+            ensure_ascii=self.ensure_ascii,
+            allow_nan=not self.strict,
+            separators=separators
+        )
+        ret = ret.replace('\u2028', '\\u2028').replace('\u2029', '\\u2029')
+        return ret.encode()
