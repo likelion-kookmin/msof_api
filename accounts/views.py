19722496845140849b65fd6ad613738c13e9e0b3
shinkeonkim <singun11@kookmin.ac.kr>
2021-07-24 22:32:35 +0900

    fix: 의미없는 UserViewSet 삭제

--- accounts/views.py
+++ accounts/views.py
@@ -5,19 +5,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 
 from .models import University
-from .serializers import UniversitySerializer, UserSerializer
-
-User = get_user_model()
-
-
-class UserViewSet(viewsets.ModelViewSet):  # pylint: disable=R0901&R0903
-    """UserViewSet
-    User 모델에 대한 기본적인 GET,POST,PUT,DELETE 메소드 지원
-    ModelViewSet 자체가 많은 view 관련 class를 상속받기 때문에
-    pylint: disable=R0901를 안 걸어주면 pylint에서 many ancenstor 걸림
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+from .serializers import UniversitySerializer
 
 class UniversityViewSet(viewsets.ModelViewSet):  # pylint: disable=R0901&R0903
     """UserViewSet
